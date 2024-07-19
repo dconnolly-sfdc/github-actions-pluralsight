b3b96e37675acea0fa619b4852b42a4b974ed7a6
dconnolly-sfdc <dconnolly@salesforce.com>
2024-07-19 16:45:44 +0100

    Update drop_rules_create.py

--- scripts/drop_rules_create.py
+++ scripts/drop_rules_create.py
@@ -1,1 +1,27 @@
 '''Script to generate terraform file with drop rules for an application, based on application name as input'''
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--application_name', required=True, help='name of non compliant application')
+
+args = parser.parse_args()
+
+# Define the variables to substitute
+variables = {
+    'application_name': args.application_name
+}
+# Define the template and output file paths
+template_file = 'drop_rules_template.tf'
+output_file = f'drop_rule_{variables["application_name"]}.tf'
+
+# Read the template file
+with open(template_file, 'r') as file:
+    template_content = file.read()
+    # Substitute the variables in the template
+    populated_content = template_content
+    for key, value in variables.items():
+        populated_content = populated_content.replace(f'{{{{{key}}}}}', value)
+        # Write the populated content to the output file
+    with open(output_file, 'w') as file:
+        file.write(populated_content)
+        print(f'Template populated and saved to {output_file}')
